e3a72fc70ae3edc9b4213ca9ce554d143f9afb52
smileym4n-collab <smileym4n@gmail.com>
2026-03-15 16:27:53 +0000

    Update lm1971_volume_control.ino

--- attiny/412/LM1971/lm1971_volume_control.ino
+++ attiny/412/LM1971/lm1971_volume_control.ino
@@ -1,1 +1,12 @@
+/*
+  LM1971 Volume Control
+  Target: ATtiny412
+  Framework: Arduino / megaTinyCore
 
+  Pin map
+PA0 -> UPDI header
+PA1 -> Potentiometer wiper
+PA2 -> LM1971 DATA
+PA3 -> LM1971 CLOCK
+PA6 -> LM1971 LOAD
+*/
